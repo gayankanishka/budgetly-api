fd092c5fa106b8e6e4f924e66bbd37e71a448a7a
Gayan Kanishka Wijetunga <32380979+gayankanishka@users.noreply.github.com>
2022-01-01 11:23:11 +0530

    add startup configs

--- src/API/Program.cs
+++ src/API/Program.cs
@@ -1,3 +1,6 @@
+using Budgetly.Application;
+using Budgetly.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +10,18 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddHealthChecks();
+
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure(builder.Configuration);
+
+builder.Services
+    .AddCors(o =>
+        o.AddDefaultPolicy(b =>
+            b.AllowAnyHeader()
+                .AllowAnyMethod()
+                .AllowAnyOrigin()));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -15,6 +30,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors();
+
+app.MapHealthChecks("/healthz");
 
 app.UseHttpsRedirection();
 
