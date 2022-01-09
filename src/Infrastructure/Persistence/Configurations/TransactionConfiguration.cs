c7c4379a96a0147435e9f4455165f58794a47cd6
Gayan Kanishka Wijetunga <32380979+gayankanishka@users.noreply.github.com>
2022-01-09 22:03:51 +0530

    feat: add more seed data

--- src/Infrastructure/Persistence/Configurations/TransactionConfiguration.cs
+++ src/Infrastructure/Persistence/Configurations/TransactionConfiguration.cs
@@ -14,7 +14,7 @@ public class TransactionConfiguration : IEntityTypeConfiguration<Transaction>
                 Id = 1,
                 Name = "Water bill",
                 Amount = 1500,
-                DateTime = new DateTime(2020, 1, 1),
+                DateTime = new DateTime(2022, 1, 1),
                 CategoryId = 4,
                 UserId = "auth0|61d9c13ff98384007046a028"
             },
@@ -23,7 +23,7 @@ public class TransactionConfiguration : IEntityTypeConfiguration<Transaction>
                 Id = 2,
                 Name = "Electricity bill",
                 Amount = 4500,
-                DateTime = new DateTime(2020, 1, 5),
+                DateTime = new DateTime(2022, 1, 5),
                 CategoryId = 4,
                 UserId = "auth0|61d9c13ff98384007046a028"
             },
@@ -32,7 +32,7 @@ public class TransactionConfiguration : IEntityTypeConfiguration<Transaction>
                 Id = 3,
                 Name = "Rent",
                 Amount = 30000,
-                DateTime = new DateTime(2020, 1, 8),
+                DateTime = new DateTime(2022, 1, 8),
                 CategoryId = 1,
                 UserId = "auth0|61d9c13ff98384007046a028"
             },
@@ -41,7 +41,7 @@ public class TransactionConfiguration : IEntityTypeConfiguration<Transaction>
                 Id = 4,
                 Name = "Groceries",
                 Amount = 23000,
-                DateTime = new DateTime(2020, 1, 19),
+                DateTime = new DateTime(2022, 1, 19),
                 CategoryId = 2,
                 UserId = "auth0|61d9c13ff98384007046a028"
             },
@@ -51,6 +51,159 @@ public class TransactionConfiguration : IEntityTypeConfiguration<Transaction>
                 Name = "Car payment",
                 Amount = 45000,
                 DateTime = new DateTime(2020, 1, 21),
+                CategoryId = 3,
+                UserId = "auth0|61d9c13ff98384007046a028"
+            },
+            new Transaction()
+            {
+                Id = 6,
+                Name = "Car insurance",
+                Amount = 10000,
+                DateTime = new DateTime(2022, 1, 21),
+                CategoryId = 3,
+                UserId = "auth0|61d9c13ff98384007046a028"
+            },
+            new Transaction()
+            {
+                Id = 7,
+                Name = "Car gas",
+                Amount = 5000,
+                DateTime = new DateTime(2022, 1, 21),
+                CategoryId = 3,
+                UserId = "auth0|61d9c13ff98384007046a028"
+            },
+            new Transaction()
+            {
+                Id = 8,
+                Name = "Car service",
+                Amount = 5000,
+                DateTime = new DateTime(2022, 1, 21),
+                CategoryId = 3,
+                UserId = "auth0|61d9c13ff98384007046a028"
+            },
+            new Transaction()
+            {
+                Id = 9,
+                Name = "Car oil",
+                Amount = 5000,
+                DateTime = new DateTime(2022, 1, 21),
+                CategoryId = 3,
+                UserId = "auth0|61d9c13ff98384007046a028"
+            },
+            new Transaction()
+            {
+                Id = 10,
+                Name = "Car tires",
+                Amount = 5000,
+                DateTime = new DateTime(2022, 1, 21),
+                CategoryId = 3,
+                UserId = "auth0|61d9c13ff98384007046a028"
+            },
+            new Transaction()
+            {
+                Id = 11,
+                Name = "Car wash",
+                Amount = 5000,
+                DateTime = new DateTime(2022, 1, 21),
+                CategoryId = 3,
+                UserId = "auth0|61d9c13ff98384007046a028"
+            },
+            new Transaction()
+            {
+                Id = 12,
+                Name = "Car wax",
+                Amount = 5000,
+                DateTime = new DateTime(2022, 1, 21),
+                CategoryId = 3,
+                UserId = "auth0|61d9c13ff98384007046a028"
+            },
+            new Transaction()
+            {
+                Id = 13,
+                Name = "Car tire rotation",
+                Amount = 5000,
+                DateTime = new DateTime(2022, 1, 21),
+                CategoryId = 3,
+                UserId = "auth0|61d9c13ff98384007046a028"
+            },
+            new Transaction()
+            {
+                Id = 14,
+                Name = "Car engine check",
+                Amount = 5000,
+                DateTime = new DateTime(2022, 1, 21),
+                CategoryId = 3,
+                UserId = "auth0|61d9c13ff98384007046a028"
+            },
+            new Transaction()
+            {
+                Id = 15,
+                Name = "Car tire check",
+                Amount = 5000,
+                DateTime = new DateTime(2022, 1, 21),
+                CategoryId = 3,
+                UserId = "auth0|61d9c13ff98384007046a028"
+            },
+            new Transaction()
+            {
+                Id = 16,
+                Name = "Car oil change",
+                Amount = 5000,
+                DateTime = new DateTime(2022, 1, 21),
+                CategoryId = 3,
+                UserId = "auth0|61d9c13ff98384007046a028"
+            },
+            new Transaction()
+            {
+                Id = 17,
+                Name = "Car tire rotation",
+                Amount = 5000,
+                DateTime = new DateTime(2022, 1, 21),
+                CategoryId = 3,
+                UserId = "auth0|61d9c13ff98384007046a028"
+            },
+            new Transaction()
+            {
+                Id = 18,
+                Name = "Car engine check",
+                Amount = 5000,
+                DateTime = new DateTime(2022, 1, 21),
+                CategoryId = 3,
+                UserId = "auth0|61d9c13ff98384007046a028"
+            },
+            new Transaction()
+            {
+                Id = 19,
+                Name = "Car tire check",
+                Amount = 5000,
+                DateTime = new DateTime(2022, 1, 21),
+                CategoryId = 3,
+                UserId = "auth0|61d9c13ff98384007046a028"
+            },
+            new Transaction()
+            {
+                Id = 20,
+                Name = "Car oil change",
+                Amount = 5000,
+                DateTime = new DateTime(2022, 1, 21),
+                CategoryId = 3,
+                UserId = "auth0|61d9c13ff98384007046a028"
+            },
+            new Transaction()
+            {
+                Id = 21,
+                Name = "Car tire rotation",
+                Amount = 5000,
+                DateTime = new DateTime(2022, 1, 21),
+                CategoryId = 3,
+                UserId = "auth0|61d9c13ff98384007046a028"
+            },
+            new Transaction()
+            {
+                Id = 22,
+                Name = "Car engine check",
+                Amount = 5000,
+                DateTime = new DateTime(2022, 1, 21),
                 CategoryId = 3,
                 UserId = "auth0|61d9c13ff98384007046a028"
             });
