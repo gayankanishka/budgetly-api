0edb7ba2d0328b5d6777e0186bf55be3495bde94
Gayan Kanishka Wijetunga <32380979+gayankanishka@users.noreply.github.com>
2022-01-13 00:59:27 +0530

    feat: add appinsights loggings with serilog

--- src/API/Program.cs
+++ src/API/Program.cs
@@ -3,10 +3,23 @@ using Budgetly.API.Services;
 using Budgetly.Application;
 using Budgetly.Application.Common.Interfaces;
 using Budgetly.Infrastructure;
+using Budgetly.Infrastructure.Logger;
+using Serilog;
+using LoggerConfigurationExtensions = Budgetly.Infrastructure.Logger.LoggerConfigurationExtensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+const string APPLICATION_NAME = "Budgetly-API";
+LoggerConfigurationExtensions.SetupLoggerConfiguration(APPLICATION_NAME);
+
+builder.Services.AddApplicationInsightsTelemetry();
+
+builder.Host.UseSerilog((hostBuilderContext, services, loggerConfiguration) =>
+{
+    loggerConfiguration.ConfigureBaseLogging(APPLICATION_NAME);
+    loggerConfiguration.AddApplicationInsightsLogging(services, hostBuilderContext.Configuration);
+});
 
 builder.Services.AddControllers(options => options.Filters.Add<ApiExceptionFilterAttribute>());
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
