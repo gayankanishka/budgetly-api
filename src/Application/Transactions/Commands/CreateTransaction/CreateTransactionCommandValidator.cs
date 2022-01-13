c5214634aa4bc00c64f097ceb1da8c4a2cc49ec3
Gayan Kanishka Wijetunga <32380979+gayankanishka@users.noreply.github.com>
2022-01-13 23:12:16 +0530

    feat: add validation for transactions

---
+++ src/Application/Transactions/Commands/CreateTransaction/CreateTransactionCommandValidator.cs
@@ -0,0 +1,41 @@
+using FluentValidation;
+
+namespace Budgetly.Application.Transactions.Commands.CreateTransaction;
+
+public class CreateTransactionCommandValidator : AbstractValidator<CreateTransactionCommand>
+{
+    public CreateTransactionCommandValidator()
+    {
+        RuleFor(x => x.Name)
+            .MinimumLength(3)
+            .WithMessage("Name must be at least 3 characters long")
+            .MaximumLength(50)
+            .WithMessage("Name must be less than 50 characters");
+        
+        RuleFor(x => x.Amount)
+            .NotNull()
+            .WithMessage("Amount is required");
+
+        RuleFor(x => x.Type)
+            .NotNull()
+            .WithMessage("Transaction type is required")
+            .IsInEnum()
+            .WithMessage("Transaction type is invalid");
+
+        RuleFor(x => x.DateTime)
+            .NotNull()
+            .WithMessage("DateTime is not valid");
+        
+        RuleFor(x => x.Note)
+            .MaximumLength(250)
+            .WithMessage("Note must be less than 250 characters");
+
+        RuleFor(x => x.CategoryId)
+            .GreaterThanOrEqualTo(1)
+            .WithMessage("CategoryId must be a valid value");
+        
+        RuleFor(x => x.IsRecurring)
+            .NotNull()
+            .WithMessage("IsRecurring is required");
+    }
+}
