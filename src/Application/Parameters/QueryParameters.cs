820edc41a04fae51fb588ce056adbadb7dfb1204
Gayan Kanishka Wijetunga <32380979+gayankanishka@users.noreply.github.com>
2022-01-13 23:13:05 +0530

    refactor: remove deadcode

--- src/Application/Parameters/QueryParameters.cs
+++ src/Application/Parameters/QueryParameters.cs
@@ -4,8 +4,6 @@ namespace Budgetly.Application.Parameters;
 
 public class QueryParameters : PaginationParameters, IFilters
 {
-    // public virtual string OrderBy { get; set; }
-    // public virtual string Fields { get; set; }
     public string? Name { get; set; }
     public DateTimeOffset? StartDate { get; set; }
     public DateTimeOffset? EndDate { get; set; }
