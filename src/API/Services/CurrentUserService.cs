76e07064aa81456c00250e696f24020d2530ba7e
Gayan Kanishka Wijetunga <32380979+gayankanishka@users.noreply.github.com>
2022-01-14 11:26:31 +0530

    refactor: remove hardcoded token

--- src/API/Services/CurrentUserService.cs
+++ src/API/Services/CurrentUserService.cs
@@ -12,7 +12,6 @@ public class CurrentUserService : ICurrentUserService
         _httpContextAccessor = httpContextAccessor;
     }
     
-    // TODO: GK | remove hardcoded userId
-    public string? UserId => _httpContextAccessor.HttpContext?.User?.FindFirstValue(ClaimTypes.NameIdentifier) 
-                             ?? "auth0|61d9c13ff98384007046a028";
+    public string? UserId => 
+        _httpContextAccessor.HttpContext?.User?.FindFirstValue(ClaimTypes.NameIdentifier);
 }
