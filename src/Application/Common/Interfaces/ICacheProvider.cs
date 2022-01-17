1d8fea3d67f463deb49f939cca3b214b59841d12
Gayan Kanishka Wijetunga <32380979+gayankanishka@users.noreply.github.com>
2022-01-18 01:38:51 +0530

    docs: add future works comments

--- src/Application/Common/Interfaces/ICacheProvider.cs
+++ src/Application/Common/Interfaces/ICacheProvider.cs
@@ -2,5 +2,5 @@ namespace Budgetly.Application.Common.Interfaces;
 
 public interface ICacheProvider
 {
-    
+    // TODO: Ideally this will have cache operations. This would be future work.
 }
--- src/Infrastructure/Persistence/Factories/CacheFactory.cs
+++ src/Infrastructure/Persistence/Factories/CacheFactory.cs
@@ -6,16 +6,19 @@ namespace Budgetly.Infrastructure.Persistence.Factories;
 
 internal class CacheFactory : AbstractPersistenceFactory<ICacheProvider>
 {
+    // TODO: Ideally this will create a cache provider based on the configuration. This would be future work.
     public override ICacheProvider GetProvider(PersistenceOptions persistenceOptions)
     {
         var provider = persistenceOptions.CurrentCacheProviderType;
         
         if (provider == CacheProviderTypes.InMemory)
         {
+            throw new NotImplementedException();
         }
 
         if (provider == CacheProviderTypes.Redis)
         {
+            throw new NotImplementedException();
         }
 
         throw new NotImplementedException();
